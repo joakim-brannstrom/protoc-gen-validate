e3884f93de9227f714e47be8fd1be6e8a53804c8
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2024-09-12 11:43:26 +0200

    update test

--- test_proto/run.sh
+++ test_proto/run.sh
@@ -4,13 +4,14 @@ pushd ..
 ~/tmp/download/bazel/bazelisk-linux-amd64 build protoc-gen-validate --sandbox_debug
 popd
 
-protoc --proto_path=. --cpp_out=generated --validate_out="lang=cc:./generated" smurf.proto validate/validate.proto
+#protoc --proto_path=. --cpp_out=generated smurf.proto validate/validate.proto
+protoc -I ../protobuf/src --proto_path=. --cpp_out=generated --validate_out="lang=cc:./generated" smurf.proto validate/validate.proto
 
 # mv generated/smurf.pb.validate.cc generated/smurf.pb.validate.cpp
 
 autoformat --no-backup generated/smurf.pb.validate.cpp
 
-diff generated/smurf.pb.validate.cpp generated/smurf.pb.validate_blipp4.cpp || true
+diff generated/smurf.pb.validate.cpp generated/smurf.pb.validate_blipp5.cpp || true
 
 time g++ -g -std=c++20 -o main main.cpp validate/validate.cpp generated/smurf.pb.cc generated/smurf.pb.validate.cpp generated/validate/validate.pb.cc -I generated/ -I .. -lprotobuf
 ./main
